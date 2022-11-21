17611199d01cc6f4983503c4a55b25bb12f08db3
ziondayoo <adetunjitemidayo26@gmail.com>
2022-11-21 13:40:30 +0100

    Add project files.

---
+++ SuperHeroAPI/Controllers/SuperHeroController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SuperHeroAPI.Models;
+
+namespace SuperHeroAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroController : ControllerBase
+    {
+
+        private static List<SuperHero> superHeroes = new List<SuperHero>
+        {
+                new SuperHero
+                {
+                    Id = 1,
+                    Name = "Spider Man",
+                    FirstName = "Peter",
+                    LastName = "Parker",
+                    Place = "New York City"
+                },
+                    new SuperHero
+                {
+                    Id = 2,
+                    Name = "Iron Man",
+                    FirstName = "Tony",
+                    LastName = "Stark",
+                    Place = "Malibu"
+                }
+        };
+
+        [HttpGet]
+        public async Task<ActionResult< List<SuperHero>>>GetAllHeroes()
+        {
+            return Ok(superHeroes);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<List<SuperHero>>> GetSinglelHero(int id)
+        {
+            var hero = superHeroes.Find(x => x.Id == id);
+            return Ok(hero);
+        }
+    }
+}
---
+++ SuperHeroAPI/Models/SuperHero.cs
@@ -0,0 +1,11 @@
+﻿namespace SuperHeroAPI.Models
+{
+    public class SuperHero
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = String.Empty;
+        public string FirstName { get; set; } = String.Empty;
+        public string LastName { get; set; } = String.Empty;
+        public string Place { get; set; } = String.Empty;
+    }
+}
